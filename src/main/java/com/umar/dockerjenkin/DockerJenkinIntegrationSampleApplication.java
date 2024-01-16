55032edcbfa736998ed8478e789dd2fe7ef26e58
umar252000 <mansuriumar86436@gmail.com>
2024-01-16 08:50:22 +0530

    intigrated

---
+++ src/main/java/com/umar/dockerjenkin/DockerJenkinIntegrationSampleApplication.java
@@ -0,0 +1,13 @@
+package com.umar.dockerjenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinIntegrationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinIntegrationSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umar/dockerjenkin/DockerJenkinIntegrationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umar.dockerjenkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinIntegrationSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
